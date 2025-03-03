fa63f46a5232d4177a7abaeaf160efddb2a8b246
Lawson Higgins <lawsoncadehiggins040409@gmail.com>
2025-03-02 17:42:19 -0700

    final_commit2

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,11 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
-import disableReactDevTools from '@fvilers/disable-react-devtools';
-
-if(process.env.NODE_ENV === 'production') {
-  disableReactDevTools();
-}
 
 ReactDOM.render(
   <React.StrictMode>
