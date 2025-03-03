29a8631ce5c618ef86eeefc137d2541fdec7d31b
Lawson Higgins <71578019+lawsonisthebest@users.noreply.github.com>
2025-03-02 19:34:33 -0700

    Update ListItem.jsx

--- frontend/src/components/ListItem.jsx
+++ frontend/src/components/ListItem.jsx
@@ -34,7 +34,7 @@ const ListItem = ({task}) => {
   }
 
   return (
-    <div className={`flex p-6 bg-blue-500 text-white rounded-lg shadow-md justify-between items-center px-8 w-full just opacity-${task.completed ? '50' : '100'}`}>
+    <div className={`flex p-6 bg-blue-500 text-white rounded-lg shadow-md justify-between items-center px-8 w-full opacity-${task.completed ? '50' : '100'}`}>
       <div className='flex items-center space-x-4'>
         <button onClick={()=>{toggleCompletion(task._id)}}>{!task.completed && <i class="fa-regular fa-circle text-2xl"></i>}{task.completed && <i class="fa-regular fa-circle-check text-2xl"></i>}</button>
         <h1 className='text-3xl font-bold break-all w-full pr-4'>{task.task}</h1>
