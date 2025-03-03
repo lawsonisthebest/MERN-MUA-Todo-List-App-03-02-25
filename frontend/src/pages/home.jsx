2bec5a03b7a0f5320c555fae80faa0671e723479
Lawson Higgins <71578019+lawsonisthebest@users.noreply.github.com>
2025-03-02 19:22:47 -0700

    Update home.jsx

--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -6,6 +6,7 @@ import PopupForm from '../components/PopupForm';
 
 const Home = () => {
     const [data, setData] = useState([]);
+
     const token = localStorage.getItem('token'); // Get the token from localStorage
 
     // Fetch tasks from the backend
@@ -29,26 +30,21 @@ const Home = () => {
         }
     };
 
-    // Call fetchTasks initially when the component mounts
     useEffect(() => {
-        fetchTasks();
+        fetchTasks(); // Fetch tasks when the component mounts
     }, []);
 
-    // Function to add new task to the list without re-fetching
-    const addTaskToList = (newTask) => {
-        setData((prevData) => [...prevData, newTask]); // Append the new task to the current task list
-    };
 
     return (
         <div className="App">
             <PopupProvider>
                 <Navbar />
-                <div className="flex justify-center items-center flex-col space-y-8 p-8 max-w-3xl mx-auto">
+                <div className='flex justify-center items-center flex-col space-y-8 p-8 max-w-3xl mx-auto'> 
                     {data.map((task) => (
                         <ListItem key={task._id} task={task} />
                     ))}
                 </div>
-                <PopupForm addTaskToList={addTaskToList} />
+                <PopupForm />
             </PopupProvider>
         </div>
     );
